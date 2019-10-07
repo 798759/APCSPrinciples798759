8c7e9743f0b1d97085982f9c9fa4067771060dd9
cspackman <42615796+cspackman@users.noreply.github.com>
2019-10-07 07:56:25 -0700

    v

---
+++ Lab 104 MySort/sketch.js
@@ -0,0 +1,31 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+var ray = [];
+var sort = [];
+var big = -1000;
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
+
+function mySort(){
+  for(var i=0; i>ray.length; i++){
+    for(var i=0; i>ray.length; i++){
+      if(i>big){
+      big=i;
+      }
+sort[ray.length-1]=big;
+    }
+  }
+}
