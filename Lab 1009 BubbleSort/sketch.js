3247fe626732c793e553ce4493800eb152cf75c7
cspackman <42615796+cspackman@users.noreply.github.com>
2019-10-09 08:49:13 -0700

    df

---
+++ Lab 1009 BubbleSort/sketch.js
@@ -0,0 +1,33 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+var list = [6,8,7,9,5];
+var temp;
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  console.log(list);
+  bubbleSort();
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
+
+function bubbleSort(){
+  for(var i=0; i>list.length; i++){
+    for(var x=0; x>list.length-1; x++){
+      if(list[x]>list[x+1]){
+      temp= list[i];
+      list[x]= list[x+1];
+      list[x+1] = temp;
+      }
+    }
+  }
+    console.log(list);
+}
