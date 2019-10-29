e41cf7dd85939adb4d982e85feff6eecf115d905
cspackman <42615796+cspackman@users.noreply.github.com>
2019-10-29 08:47:10 -0700

    de

--- Project moving bars/Bar.js
+++ Project moving bars/Bar.js
@@ -1,9 +1,13 @@
 
 class Bar{
-  constructor(x,y,w,h,c){
+  constructor(x,y,w,h,c,r,g,b){
     this.loc=createVector(x,y);
     this.w=w;
     this.h=h;
+    this.r=r;
+    this.g=g;
+    this.b=b;
+    this.avrg = (this.b+this.g+this.r)/3
   }
 run(){
 
@@ -11,8 +15,9 @@ run(){
 
 }
 render(){
-  fill(255,23,201);
+  fill(this.r,this.g,this.b);
   rect(this.loc.x,this.loc.y,this.w,this.h);
+  //fill(this.r,this.g,this.b);
 }
 set(i){
   this.loc.x=i*25;
--- Project moving bars/sketch.js
+++ Project moving bars/sketch.js
@@ -6,7 +6,8 @@ function setup(){
   var cnv=createCanvas(800,800);
   cnv.position((windowWidth-width)/2,30);
   background(235);
-  loadBars();
+//  loadBars();
+loadBarsColor();
   frameRate(5);
   for(var i=0; i<bars.length; i++){
     bars[i].run();
@@ -14,7 +15,8 @@ function setup(){
 }
 
 function draw(){
-bubblesort();
+//bubblesort();
+bubblesortColor();
 }
 
 function move(){
@@ -34,17 +36,34 @@ function bubblesort(){
       }
     }
   }
-
+  function bubblesortColor(){
+      for (var j=0; j<bars.length-1; j++){
+        if(bars[j].avrg>bars[j+1].avrg){
+          swap(bars, j, j+1);
+          move();
+        }
+      }
+    }
 
 
 
 function loadBars(){
   for (var x=0; x<32; x++){
     var w=int(random(1,32));
-    bars[x]= new Bar(25*x,800-(25*w),25,(25*w))
+    r=255;
+    b=23;
+    g=201;
+    bars[x]= new Bar(25*x,800-(25*w),25,(25*w),r,g,b)
   }
 }
-
+function loadBarsColor(){
+  for (var x=0; x<32; x++){
+    var r=int(random(1,255));
+    var b=int(random(1,255));
+    var g=int(random(1,255));
+    bars[x]= new Bar(25*x,800-(200),25,(700),r,g,b)
+  }
+}
 function swap(list,a,b){
   var temp=list[a];
   list[a]=list[b];
